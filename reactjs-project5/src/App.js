7537c7e4b139da520aa329849d872e011945ae85
masum-yasin <masum55549@gmail.com>
2023-11-12 18:59:41 +0600

    update

--- reactjs-project5/src/App.js
+++ reactjs-project5/src/App.js
@@ -1,15 +1,47 @@
 import logo from './logo.svg';
 import './App.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 function App() {
   return (
     <div className="App">
+      <TodoPost></TodoPost>
       <District name='samsung' laptop='asus'></District>
       <District name='sony' laptop='lenovo'></District>
       <District name='walton' laptop='mac'></District>
     </div>
   );
+}
+function TodoPost(){
+  const [todolist,SetTodoList] = useState([]);
+  useEffect(()=>{
+    fetch("https://jsonplaceholder.typicode.com/todos")
+   .then(res => res.json())
+   .then(data => SetTodoList(data))
+   
+  },[])
+
+
+  return(
+    <div>
+      <h1>These are here is Todo List:{todolist.length}</h1>
+      <div>
+        {
+          todolist.map(todo=> <SingleTodolist id={todo.id} title={todo.title} complete={todo.completed} ></SingleTodolist> )
+        }
+      </div>
+    </div>
+  )
+}
+function SingleTodolist(props){
+  return (
+    <div style={{backgroundColor:'Black', color:'yellow',  textAlign:'center'}}>
+      <h2>ID:{props.id}</h2>
+      <h5>Title:{props.title}</h5>
+      <p>{props.userId}</p>
+      <h5>Completed:{props.complete}</h5>
+    </div>
+  )
 }
 const districtStyle ={
   backgroundColor:'darkBlue',
