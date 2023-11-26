3f30fe7c369f367d25ade5b255c2b20d2f271718
masum-yasin <masum55549@gmail.com>
2023-11-25 18:28:29 -0800

    update

--- ema-john-project2/src/Component/Product/Product.jsx
+++ ema-john-project2/src/Component/Product/Product.jsx
@@ -2,15 +2,15 @@ import React from 'react';
 import './Product.css';
 
 const Product = (props) => {
-    const {img,name,seller,qunatity,price} =props.product
+    const {img,name,seller,quantity,price} =props.product
     // console.log(props);
     return (
         <div className='product'>
           <img src={img} alt="" />
-          <h4>Name:{name}</h4>
+          <h6 className='product-name'>Name:{name}</h6>
           <p>Seller:{seller}</p>
           <p>Price:{price}</p> 
-          <p>Quantity:{qunatity}</p>
+          <p>Quantity:{quantity}</p>
         </div>
     );
 };
