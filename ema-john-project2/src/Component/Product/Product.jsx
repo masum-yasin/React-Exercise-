492b075f388ac85c97f14ece0b9697f26ce16dd6
masum-yasin <masum55549@gmail.com>
2023-11-27 18:02:51 -0800

    update

--- ema-john-project2/src/Component/Product/Product.jsx
+++ ema-john-project2/src/Component/Product/Product.jsx
@@ -7,10 +7,13 @@ const Product = (props) => {
     return (
         <div className='product'>
           <img src={img} alt="" />
+          <div className='product-info'>
           <h6 className='product-name'>Name:{name}</h6>
-          <p>Seller:{seller}</p>
+          <p>Manufacturer:{seller}</p>
           <p>Price:{price}</p> 
           <p>Quantity:{quantity}</p>
+        </div>
+          <button className='btn-cart'>Add to Cart</button>
         </div>
     );
 };
