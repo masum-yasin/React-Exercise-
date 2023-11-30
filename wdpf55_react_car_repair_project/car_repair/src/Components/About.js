f940aa297e9cdce82266c6534ca05f72c3c66c0f
masum-yasin <masum55549@gmail.com>
2023-11-30 19:09:37 +0600

    update

--- wdpf55_react_car_repair_project/car_repair/src/Components/About.js
+++ wdpf55_react_car_repair_project/car_repair/src/Components/About.js
@@ -8,7 +8,7 @@ const About = () => {
             <div className="row g-5">
                 <div className="col-lg-6 pt-4" style={{minHig: "400px"}}>
                     <div className="position-relative h-100 wow fadeIn" data-wow-delay="0.1s">
-                        <img className="img-fluid w-100 h-100" src="assets/img/about.jpg" style={{objectFit: "cover" }}alt=""/>
+                        <img className="img-fluid w-100 h-75" src="assets/img/about_img2.jpg" style={{objectFit: "cover" }}alt=""/>
                         <div className="position-absolute top-0 end-0 mt-n4 me-n4 py-4 px-5" style={{background:" rgba(0, 0, 0, .08)"}}>
                             <h1 className="display-4 text-white mb-0">15 <span className="fs-4">Years</span></h1>
                             <h4 className="text-white">Experience</h4>
@@ -16,7 +16,7 @@ const About = () => {
                     </div>
                 </div>
                 <div className="col-lg-6">
-                    <h6 className="text-primary text-uppercase">// About Us //</h6>
+                    <h2 className="text-primary text-uppercase">// About Us //</h2>
                     <h1 className="mb-4"><span className="text-primary">CarServ</span> Is The Best Place For Your Auto Care</h1>
                     <p className="mb-4">Tempor erat elitr rebum at clita. Diam dolor diam ipsum sit. Aliqu diam amet diam et eos. Clita erat ipsum et lorem et sit, sed stet lorem sit clita duo justo magna dolore erat amet</p>
                     <div className="row g-4 mb-3 pb-3">
