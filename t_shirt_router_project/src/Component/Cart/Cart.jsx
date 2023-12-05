fd2060221329eddad45195a6401a9da0318dcf4c
masum-yasin <masum55549@gmail.com>
2023-12-05 19:12:02 +0600

    update

--- t_shirt_router_project/src/Component/Cart/Cart.jsx
+++ t_shirt_router_project/src/Component/Cart/Cart.jsx
@@ -1,12 +1,25 @@
 import React from 'react';
 
-const Cart = ({cart}) => {
+
+const Cart = ({cart,handleRemoveFormCart}) => {
+  let message;
+  if(cart.length ==0){
+    message = <p>You added a t-shirt</p>
+  }
+  else{
+    message = <div>
+      <h3>Boroloxxxx</h3>
+      <span>Thanks you please wisting your money</span>
+    </div>
+  }
     return (
         <div>
           <h4>Order Summary:{cart.length}</h4>
+           {cart.length>2 ? <span>You are Allow Discount</span>:<span>No discount Allow</span>}
+               <h3 style={{color:'red'}}>{message}</h3>
           {
-            cart.map(tshirt =><p key={tshirt._id}>{tshirt.name}
-            <button onClick={()=>handleRemoveFormCart(tshirt._id)}>Remove Item</button>
+            cart.map(shirt =><p key={shirt._id}>{shirt.name}
+            <button onClick={()=>handleRemoveFormCart(shirt._id)}>Remove Item</button>
             </p>)
           }
         </div>
@@ -14,3 +27,8 @@ const Cart = ({cart}) => {
 };
 
 export default Cart;
+/* 
+// CONDITIONAL RENDERING//
+1. use if else to set a variable that will contain in element, components 
+2. tarnary operator: conditional ? 'true':'false';
+*/
--- t_shirt_router_project/src/Component/Home/Home.jsx
+++ t_shirt_router_project/src/Component/Home/Home.jsx
@@ -3,17 +3,26 @@ import {useLoaderData} from 'react-router-dom'
 import TShirt from '../TShirt/TShirt';
 import Cart from '../Cart/Cart';
 import './Home.css';
+import toast  from 'react-hot-toast';
 
 
 const Home = () => {
     const tshirts = useLoaderData();
     const [cart,setCart] = useState([]);
     const handleAddtoCart = (shirt)=>{
-        const newCart = [...cart,shirt];
+        const exists = cart.find(ts =>ts._id===shirt._id)
+        if(exists){
+            toast("you have already added this t_shirt")
+        }
+        else{
+            const newCart = [...cart,shirt];
         setCart(newCart)
+        }
+        
     };
-    const handleRemoveFormCart =(id)=>{
-        console.log(id);
+    const handleRemoveFormCart = (id)=>{
+        const remaining = cart.filter(ts =>ts._id !==id);
+        setCart(remaining);
     } 
 
    
