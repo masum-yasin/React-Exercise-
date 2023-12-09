02842b6b08c77eb64afd246cf9ab817116f3f5c0
masum-yasin <masum55549@gmail.com>
2023-12-09 19:12:44 +0600

    update

--- High_School_website/src/Component/AwesomeFeatures/AwesomeFeatures.jsx
+++ High_School_website/src/Component/AwesomeFeatures/AwesomeFeatures.jsx
@@ -8,25 +8,25 @@ const AwesomeFeatures = () => {
            <p className=' text-center mb-5'>Replenish man have thing gathering lights yielding shall you</p>
            <div className="card-group">
   <div className="card">
-    <img src="..." className="card-img-top" alt="..." />
+    <img src={'img/features/f1.png'} className="card-img-top" alt="..." />
     <div className="card-body">
-      <h5 className="card-title">Card title</h5>
-      <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      <h5 className="card-title">Scholarship Facility</h5>
+      <p className="card-text">If you’re applying for a scholarship, chances are you are going to need to write an essay. Very few scholarship programs are based solely on an application form or transcript.</p>
       <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
     </div>
   </div>
   <div className="card">
-    <img src="..." className="card-img-top" alt="..." />
+    <img src={'img/features/f2.png'} className="card-img-top" alt="..." />
     <div className="card-body">
-      <h5 className="card-title">Card title</h5>
-      <p className="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+      <h5 className="card-title">Sell Online Course</h5>
+      <p className="card-text">Today, almost every person has heard about online classes. Previously, only very few educational institutions offered online classes for a few courses.</p>
       <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
     </div>
   </div>
   <div className="card">
-    <img src="..." className="card-img-top" alt="..." />
+    <img src={'img/features/f3.png'} className="card-img-top" alt="..." />
     <div className="card-body">
-      <h5 className="card-title">Card title</h5>
+      <h5 className="card-title">Global Certification</h5>
       <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
       <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
     </div>
