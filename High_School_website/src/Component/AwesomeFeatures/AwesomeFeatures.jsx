55c7d1026aabbfc9226fad0688b3d74ff5c483cb
masum-yasin <masum55549@gmail.com>
2023-12-09 18:23:39 -0800

    update

--- High_School_website/src/Component/AwesomeFeatures/AwesomeFeatures.jsx
+++ High_School_website/src/Component/AwesomeFeatures/AwesomeFeatures.jsx
@@ -7,27 +7,27 @@ const AwesomeFeatures = () => {
            <h1 className='awesometitle fw-bolder text-bg-dark text-center'>Awesome Feature</h1>
            <p className=' text-center mb-5'>Replenish man have thing gathering lights yielding shall you</p>
            <div className="card-group">
-  <div className="card">
+  <div className=" awesome-card card">
     <img src={'img/features/f1.png'} className="card-img-top" alt="..." />
     <div className="card-body">
-      <h5 className="card-title">Scholarship Facility</h5>
-      <p className="card-text">If you’re applying for a scholarship, chances are you are going to need to write an essay. Very few scholarship programs are based solely on an application form or transcript.</p>
+      <h5 className="card-title fw-bold ">Scholarship Facility</h5>
+      <p className="card-text fst-italic">If you’re applying for a scholarship, chances are you are going to need to write an essay. Very few scholarship programs are based solely on an application form or transcript.</p>
       <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
     </div>
   </div>
-  <div className="card">
+  <div className="awesome-card card">
     <img src={'img/features/f2.png'} className="card-img-top" alt="..." />
     <div className="card-body">
-      <h5 className="card-title">Sell Online Course</h5>
-      <p className="card-text">Today, almost every person has heard about online classes. Previously, only very few educational institutions offered online classes for a few courses.</p>
+      <h5 className="card-title fw-bold">Sell Online Course</h5>
+      <p className="card-text fst-italic">Today, almost every person has heard about online classes. Previously, only very few educational institutions offered online classes for a few courses.</p>
       <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
     </div>
   </div>
-  <div className="card">
+  <div className=" awesome-card card">
     <img src={'img/features/f3.png'} className="card-img-top" alt="..." />
     <div className="card-body">
-      <h5 className="card-title">Global Certification</h5>
-      <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+      <h5 className="card-title fw-bold">Global Certification</h5>
+      <p className="card-text fst-italic">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
       <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
     </div>
   </div>
--- High_School_website/src/Pages/Home.jsx
+++ High_School_website/src/Pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Banner from '../Component/Banner/Banner';
 import AwesomeFeatures from '../Component/AwesomeFeatures/AwesomeFeatures';
+import Course from '../Component/Courese/Course';
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
             <AwesomeFeatures/>
+            <Course/>
             
         </div>
     );
