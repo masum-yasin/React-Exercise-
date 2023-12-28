e5922a0eab8be1ab00b99f3245d8394be0a20423
masum-yasin <masum55549@gmail.com>
2023-12-28 09:38:33 +0600

    update

--- High_School_website/src/Component/Blog/BlogC.jsx
+++ High_School_website/src/Component/Blog/BlogC.jsx
@@ -1,10 +1,39 @@
 import React from 'react';
+import './BlogC.css'
+import Breadcrumb from '../Breadcrumb/Breadcrumb';
+
 
 const BlogC = () => {
     return (
-        <div>
-            <h1>this si blog page</h1>
+      <>
+        {/* header img start */}
+        <div className='blog-header-img'>
+          <img src={'img/banner/pexels-keira-burton-6146978.jpg'} alt="" />
+          {/* header image ending*/}
+        <div className='blog-breadcrumb mt-5 border-5  text-center'>
+                <Breadcrumb></Breadcrumb>
+                <hr />
+       </div>
+       <div className='blog-heading'><h1>Our Education Blog</h1></div>
+    </div>
+    <div className="row">
+        <div className="container col-lg-8 mt-5">
+     <img src={'img/blog/blog1-770x450 (1).jpg'} alt="" />
         </div>
+        <div className="col-lg-4 mt-5">
+        <form className="d-flex">
+        
+  <div className='search-form'>
+    <h4>Search Form</h4>
+    <input classname=" form-control me-2" type="search" placeholder="Here are Search" aria-label="Search" />
+    <button classname="btn btn-outline-primary" type="submit">Search</button>
+  </div>
+</form>
+
+
+        </div>
+    </div>
+      </>
     );
 };
 
